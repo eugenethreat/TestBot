b38bb41cbbe22ebc4372649d47a8ccc37aada580
Eugene Ryoo <45939110+eugenethreat@users.noreply.github.com>
2019-11-02 20:27:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def msg_received_from_group():
   elif data['text'].lower() == "!testpic":
     send_msg_pic("Hello World!","https://i.groupme.com/1024x1024.jpeg.d733d6de5c36462f8d1cb67e3191b618")
 
-  elif data['text'].contains('a'):
+  elif "a" in data['text']:
     send_msg("AAAAAAAAAAAAAAAAAAAAAAAaa")
 	
 
